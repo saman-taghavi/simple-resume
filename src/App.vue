63eabd5e60d3109ca8db56edb637d3b5b82ce0e9
Saman Taghavi <samantaghavisani@gmail.com>
2022-02-25 01:17:53 +0330

    Add heading

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<script setup lang="ts"></script>
-
+<script setup lang="ts">
+let src =
+  "https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=4&w=256&h=256&q=80";
+</script>
 <template>
-  <div class="text-3xl font-bold underline text-center">hi there!</div>
+  <!-- start of heading -->
+  <div name="heading" class="bg-blue-600 flex gap-4 p-4">
+    <div class="flex flex-col items-center mx-auto">
+      <img class="rounded-full w-36 mt-4" :src="src" />
+      <strong class="mt-4">Andrew Alfred</strong>
+      <span class="mt-8">Technical advisor</span>
+    </div>
+  </div>
+  <!-- end of heading -->
 </template>
